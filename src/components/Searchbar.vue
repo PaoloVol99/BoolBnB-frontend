0feda92347a96d1148357fbc6d551ca808d84299
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-05-27 19:57:31 +0200

    FEAT custom searchbar

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,69 +1,134 @@
 <template>
     <div>
-
-        <div v-html="searchBoxHTML">
+        <div class="container">
+            <div class="ms_form-container position-relative">
+                <input class="ms_input" v-model="searchBox" @keyup="fetchResults()" type="text">
+                <div class="ms_autocomplete-container position-absolute">
+                    <div class="autocomplete" v-for="(result, i) in results" @click="selectResult(i)">
+                        {{ result.address.freeformAddress }}
+                    </div>
+                </div>
+                <button class="btn btn-primary rounded-pill" @click="fetchApartments()">Cerca</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import tt from '@tomtom-international/web-sdk-maps';
-import { services } from '@tomtom-international/web-sdk-services';
-import SearchBox from '@tomtom-international/web-sdk-plugin-searchbox';
-
-const options = {
-
-    searchOptions: {
-    key: "5yE1GYuQA7WyAdPZ1zAeJtBq8cKtoae3",
-    language: "it-IT",
-    limit: 5,
-    countrySet: "IT"
-    },
-    autocompleteOptions: {
-    key: "5yE1GYuQA7WyAdPZ1zAeJtBq8cKtoae3",
-    language: "it-IT",
-    countrySet: "IT"
-    }
-}
-
-const ttSearchBox = new SearchBox(services, options);
-const searchBoxHTML = ttSearchBox.getSearchBoxHTML();
-// document.getElementById('giuppi').append(searchBoxHTML)
-
-// map.addControl(ttSearchBox, 'top-left');
-
+import axios from 'axios'
     export default {
         data() {
             return {
+                searchBox: '',
+                results: [],
             }
         },
         methods: { 
-        //     searchBar() {
-        //         let options = {
-        //         searchOptions: {
-        //         key: "5yE1GYuQA7WyAdPZ1zAeJtBq8cKtoae3",
-        //         language: "it-IT",
-        //         limit: 5,
-        //         countrySet: "IT"
-        //         },
-        //         autocompleteOptions: {
-        //         key: "5yE1GYuQA7WyAdPZ1zAeJtBq8cKtoae3",
-        //         language: "it-IT",
-        //         countrySet: "IT"
-        //         },
-        //         }
-        //     let ttSearchBox = new tt.plugins.SearchBox(tt.services, options)
-        //     let searchBoxHTML = ttSearchBox.getSearchBoxHTML()
-        //     document.body.append(searchBoxHTML)
-        //     console.log(document)
-        //     },
-        // },
-        // mounted() {
-        //     this.searchBar()
-        // }
-        }}
+            fetchResults() {
+                if (this.searchBox != '') {
+                axios.get(`https://api.tomtom.com/search/2/search/${this.searchBox}.json`, 
+                {
+                    params: {
+                        key: '5yE1GYuQA7WyAdPZ1zAeJtBq8cKtoae3',
+                        countrySet: 'IT',
+                        language: 'it-IT',
+                        limit: 5
+                    }
+                })
+                .then ((res) => {
+                    console.log(res.data.results)
+                    this.results = res.data.results
+                })
+            } else {
+                this.results = []
+            }},
+            selectResult(i) {
+                let result = this.results[i].address.freeformAddress
+                console.log(result)
+                this.searchBox = result
+                this.results = []
+
+
+            },
+            // fetchApartments() {
+            //     axios.get('')
+            // }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables.scss' as *;
+
+    .ms_form-container {
+        display: flex;
+        align-items: center;
+        margin: 0 auto;
+    }
+
+    .ms_autocomplete-container {
+        width: 90%;
+        left: 20px;
+        top: 80px;
+        margin-top: -18px;
+    }
+    .ms_input {
+        width: 90%;
+        margin: 20px;
+        display: block;
+        background-color: rgb(234, 234, 234);
+        border: none;
+        color: black;
+        border-radius: 999px;
+        padding: 8px 25px;
+
+        &:focus-visible {
+            outline: 2px solid $primary-color;
+        }
+    }
+    .autocomplete {
+        // width: 90%;
+        // margin: 0 auto;
+        display: block;
+        // border-radius: 10px;
+        // border-top: 1px solid rgb(205, 205, 205);
+        cursor: pointer;
+        padding: 8px 25px;
+        background-color: white;
+        // &::after {
+        //     content: '';
+        //     display: block;
+        //     background-color: rgb(205, 205, 205);
+        //     width: 70%;
+        //     height: 1px;
+        //     margin: 8px auto 0;
+        // }
+
+        &:first-of-type {
+            border-top: none;
+            border-radius: 10px 10px 0 0;
+        }
+
+        &:last-of-type {
+            border-bottom: 1px solid rgb(205, 205, 205);
+            margin-bottom: 10px;
+            border-radius: 0 0 10px 10px;
+        }
+    }
+
+    .autocomplete:hover {
+        // border-radius: 10px;
+        background-color: rgb(235, 235, 235);
+        padding-bottom: 8px;
+        border: none;
+
+        &+.autocomplete {
+            border: none;
+        }
+
+        // &::after {
+        //     display: none;
+        // }
+    }
 
 </style>
