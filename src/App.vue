4bf349f05fd2fd2291344037644a2b7bca586ad3
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-05-29 09:06:15 +0200

    FEAT featured and suggested section added

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import Searchbar from './components/Searchbar.vue'
+import Searchbar from './components/Searchbar.vue';
+import Suggested_holidays from './components/Suggested_holidays.vue';
+import Featured_accommodation from './components/Featured_accommodation.vue';
 
 export default {
   
   components: {
     Footer,
     Header,
-    Searchbar
+    Searchbar,
+    Suggested_holidays,
+    Featured_accommodation
   },
   }
 </script>
@@ -19,6 +23,10 @@ export default {
   <Header />
 
   <Searchbar />
+
+  <Suggested_holidays />
+
+  <Featured_accommodation />
 
   <Footer />
 
---
+++ src/components/Featured_accommodation.vue
@@ -0,0 +1,309 @@
+<template>
+    <section class="featured_accommodation_section py-4">
+
+        <div class="container">
+            <h2 class="title py-3 text-center text-md-start">Alloggi in evidenza</h2>
+        </div>
+
+        <div class="container">
+            <div class="row justify-content-center">
+                
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" src="/firenze.png" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Casa bella</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                
+
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables.scss' as *;
+.featured_accommodation_section{
+
+    .title{
+        font-size: 26px;
+    }
+
+    .card-spacer {
+    margin-bottom: 20px; 
+    padding: 0 20px;
+    }
+
+    .card{
+        overflow: hidden;
+        box-shadow: 0 1px 20px rgba(0, 0, 0, 0.171);
+    }
+    .card:hover{
+        .card-img{
+            transform: scale(1.1);
+            transition-duration: 0.5s;
+        }
+    }
+
+    img{
+        display: block;
+    }
+    .card-img{
+        height: 150px;
+        transition-duration: 0.5s;
+        object-position: center;
+        object-fit: cover;
+    }
+
+    .card-description{
+        position: relative;
+        bottom: 0;
+        padding: 10px;
+        background-color: white;
+    }
+    .card-title{
+        font-size: 20px;
+    }
+    .card-location{
+        font-size: 15px;
+        color: black;
+    }
+    .card-icon-summary{
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        color: black;
+        font-size: 14px;
+    }
+    .icon-info{
+        display: flex;
+    }
+
+}
+
+</style>
