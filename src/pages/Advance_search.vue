8d75a69be9b2ac026bad22cc3aa4fa330acecbbf
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-05-31 09:46:14 +0200

    FEAT search, filter and cards added

--- src/pages/Advance_search.vue
+++ src/pages/Advance_search.vue
@@ -7,16 +7,100 @@
             <h1 class="py-3">Ricerca Avanzata</h1>
         </div>
         <div class="container">
-            <p>Elenco risultati</p>
-            <p>{{ store.city}}</p>
+            <div class="ms-filters">
+                <Searchbar class="searchbar"></Searchbar>
+                <input class="input_number" type="number" id="num_ospiti" name="num_ospiti" placeholder="Numero di ospiti">
+                <a class="btn ms-other-filters ms-3" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                Altri filtri
+                </a>
+            </div>
+        </div>
 
-            <div v-for="apartment in store.apartments" :key="apartment.id" class="card" style="width: 18rem;">
-                <img class="card-img-top" :src="apartment.cover_image" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">{{apartment.title}}</h5>
-                    <p class="card-text">{{apartment.description}}</p>
+
+        <div>
+
+
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Filtri avanzati</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    cercati l'appartamento nella lista
                 </div>
             </div>
+        </div>
+
+
+
+
+
+        <div class="container py-3">
+            <div class="row justify-content-center">
+                <!-- <div v-for="apartment in store.apartments" :key="apartment.id" class="card" style="width: 18rem;">
+                    <img class="card-img-top" :src="apartment.cover_image" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">{{apartment.title}}</h5>
+                        <p class="card-text">{{apartment.description}}</p>
+                    </div>
+                </div> -->
+
+                <!-- <div v-for="apartment in store.apartments" :key="apartment.id" class="col-sm-6 col-md-4 col-lg-3 card-spacer ms-col">
+                    <div class="card">
+                        <img class="card-img" :src="apartment.cover_image" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">{{apartment.title}}</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="far fa-file-alt" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon icon="fa-regular fa-user" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div> -->
+                <div v-for="index in 12" :key="index" class="col-sm-6 col-md-4 col-lg-3 ms-col">
+                    <div class="card">
+                        <img class="card-img" src="https://picsum.photos/200/300" alt="immagine">
+                        <div class="card-description">
+                            <h4 class="card-title">Nome appartamento</h4>
+                            <p class="card-location">Napoli, NA</p>
+                            <div class="icon-info">
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon :icon="['fas', 'bed']" />
+                                    <p>5</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon :icon="['fas', 'user']" />
+                                    <p>8</p>
+                                </div>
+                                <div class="card-icon-summary">
+                                    <font-awesome-icon :icon="['fas', 'shower']" />
+                                    <p>2</p>
+                                </div>
+                                <div class="card-price-summary ms-3">
+                                    <p>23&euro;</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     </section>
 
@@ -27,6 +111,7 @@
 <script>
 import Default from '../layouts/Default.vue';
 import store from '../store';
+import Searchbar from '../components/Searchbar.vue';
 
     export default {
     data(){
@@ -35,7 +120,8 @@ import store from '../store';
         }
     },
     components: { 
-        Default 
+        Default,
+        Searchbar,
     },
     mounted() {
         
@@ -44,5 +130,87 @@ import store from '../store';
 </script>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables.scss' as *;
+.ms-filters{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+// .searchbar{
+//     margin-right: auto;
+// }
+.input_number{
+    width: 15%;
+    border-radius: 999px;
+    border: none;
+    padding: 8px 25px;
+    background-color: rgb(234, 234, 234);
+    &:focus-visible {
+        outline: 2px solid $primary-color;
+    }
+}
+.ms-other-filters{
+    background-color: $primary-color;
+    color: white;
+    border-radius: 999px;
+}
+.card{
+    overflow: hidden;
+    box-shadow: 0 1px 20px rgba(0, 0, 0, 0.171);
+    margin-bottom: 20px;
+    .card:hover{
+        .card-img{
+            transform: scale(1.1);
+            transition-duration: 0.5s;
+        }
+    }
 
+    img{
+        display: block;
+    }
+    .card-img{
+        height: 150px;
+        transition-duration: 0.5s;
+        object-position: center;
+        object-fit: cover;
+    }
+
+    .card-description{
+        position: relative;
+        bottom: 0;
+        margin: 0 auto;
+        padding: 10px;
+        background-color: white;
+    }
+    .card-title{
+        font-size: 20px;
+    }
+    .card-location{
+        font-size: 15px;
+        color: black;
+    }
+    .card-icon-summary{
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        color: black;
+        font-size: 14px;
+
+        p{
+            margin: 0;
+        }
+    }
+    .card-price-summary{
+        
+        font-size: 28px;
+        p{
+            margin: 0;
+        }
+    }
+    .icon-info{
+        display: flex;
+        gap: 20px;
+        align-items: baseline;
+    }
+}
 </style>
