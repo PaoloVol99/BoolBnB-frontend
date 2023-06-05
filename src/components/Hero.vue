c5cc76f7b8e9b6bb836cadc7ed92167d5d1f966b
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-06-05 11:41:17 +0200

    FEAT hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="hero-container">
+        <div class="black-filter">
+            <Searchbar class="searchbar" />
+
+        </div>
+    </div>
+</template>
+
+<script>
+import Searchbar from './Searchbar.vue';
+    export default {
+        components: {
+            Searchbar,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.hero-container {
+    background-image: url(/foto-sfondo-hero.jpeg);
+    height: 88vh;
+    background-position: center;
+    background-size: cover;
+    margin-bottom: 50px;
+
+    .black-filter {
+        background-color: rgba($color: #000000, $alpha: 0.3);
+        width: 100%;
+        height: 100%;
+        position: relative;
+
+        .searchbar {
+            position: absolute;
+            left: 20%;
+            right: 20%;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+
+    }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
 
 <Default>
 
-    <Searchbar />
+    <Hero />
 
     <Suggested_holidays />
 
@@ -18,13 +18,15 @@ import Default from '../layouts/Default.vue';
 import Searchbar from '../components/Searchbar.vue';
 import Suggested_holidays from '../components/Suggested_holidays.vue';
 import Featured_accommodation from '../components/Featured_accommodation.vue';
+import Hero from '../components/Hero.vue';
 
     export default {
         components:{
             Default,
             Searchbar,
             Suggested_holidays,
-            Featured_accommodation
+            Featured_accommodation,
+            Hero
         }
     }
 </script>
