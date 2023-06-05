24ca653e8e54eeb2ab9a2775c7481c9673651979
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-06-05 02:00:12 +0200

    STYLE style added

--- src/pages/ApartmentDetails.vue
+++ src/pages/ApartmentDetails.vue
@@ -2,19 +2,33 @@
     <Default>
 
         <div class="container">
-            <h1>{{ apartment.title }}</h1>
+            <h1 class="mt-3 mb-0">{{ apartment.title }}</h1>
             <span>{{ apartment.address + ', ' + apartment.city }}</span>
-            <div class="row ms_row">
-                <div class="col-xl-7 col-sm-12 ms_col-5 p-1 g-0">
+            <div class="row ms_row my-3">
+                <div class="col-xl-7 col-sm-12 ms_col-5 py-1">
                     <img class="ms_img" :src="apartment.cover_image" alt="">
                 </div>
-                <div class="col-xl-5 col-sm-12 ms_col-7 d-flex flex-wrap g-0">
+                <div class="col-xl-5 col-sm-12 ms_col-7 d-flex flex-wrap gx-3">
                     <div class="extra-image-container p-1" v-for="image in apartment.images" :class="extraImagesLayout()">
                         <img class="ms_img extra-image" :src="image" alt="">
                     </div>
                 </div>
             </div>
-            <div class="specifications d-flex">
+            <div class="icon-info d-flex align-items-center mb-4">
+                <div class="specs-icon d-flex align-items-baseline pe-2 fs-5">
+                    <font-awesome-icon class="icon-color pe-2" :icon="['fas', 'bed']" />
+                    <p class="mb-0">{{ apartment.beds }} posti letto</p>
+                </div>
+                <div class="specs-icon d-flex align-items-baseline px-2 fs-5">
+                    <font-awesome-icon class="icon-color pe-2" :icon="['fas', 'door-closed']" />
+                    <p class="mb-0">{{ apartment.rooms }} stanze</p>
+                </div>
+                <div class="specs-icon d-flex align-items-baseline px-2 fs-5">
+                    <font-awesome-icon class="icon-color pe-2" :icon="['fas', 'shower']" />
+                    <p class="mb-0">{{ apartment.bathrooms }} bagni</p>
+                </div>
+            </div>
+            <!-- <div class="specifications d-flex">
                 <span class="spec">
                     {{ apartment.beds }} posti letto
                 </span>
@@ -24,41 +38,50 @@
                 <span class="spec">
                     {{ apartment.bathrooms }} bagni
                 </span>
-            </div>
+            </div> -->
             <div class="row">
-                <div class="col-8">
+                <div class="col-xl-8 col-sm-12">
+                    <h4>Qualcosa in più sull'appartamento:</h4>
                     <p>
                         {{ apartment.description }}
                     </p>
+                    <h4 class="mt-3">Dove si trova:</h4>
+                    <div class="mb-4" id="map-container">
+    
+                    </div>
                 </div>
-                <div class="col-4">
-                    <div class="info-container">
-                        <h3> {{ apartment.price }} /notte </h3>
-                        <span>{{ apartment.sqm }} &#13217;</span>
+                <div class="col-xl-4 col-sm-12">
+                    <div class="aside-container">
+                        <div class="info-container">
+                            <h3> &euro;{{ apartment.price }} a notte </h3>
+                            <span class="fs-4">{{ apartment.sqm }} &#13217;</span>
+                        </div>
+                        <form class="messages-form" @submit.prevent="sendForm">
+                            <h4>Manda un messaggio al proprietario</h4>
+                            <div>
+                                <!-- <label for="name">Nome</label> -->
+                                <input type="text" v-model="name" id="name" name="name" placeholder="Il tuo nome">
+                            </div>
+                            <div>
+                                <!-- <label for="email">Indirizzo email</label> -->
+                                <input type="text" v-model="email" id="email" name="email" placeholder="Il tuo indirizzo email">
+                            </div>
+                            <div>
+                                <!-- <label for="text">Messaggio</label> -->
+                                <textarea type="text" v-model="text" id="text" name="text" placeholder="Scrivi il tuo messaggio"></textarea>
+                            </div>
+                            <input class="d-none" type="number" v-model="apartment.id">
+                            <button type="submit">Invia</button>
+                        </form>
+
                     </div>
                 </div>
 
-                <div id="map-container">
-
-                </div>
             </div>
 
         </div>
         <!-- <Message :apartment_id="apartment.id"></Message> -->
         <div class="container">
-            <form @submit.prevent="sendForm">
-                <div>
-                    <input type="text" v-model="name" name="name">
-                </div>
-                <div>
-                    <input type="text" v-model="email" name="email">
-                </div>
-                <div>
-                    <textarea type="text" v-model="text" name="text"></textarea>
-                </div>
-                <input class="d-none" type="number" v-model="apartment.id">
-                <button type="submit">Invia</button>
-            </form>
         </div>
 
     </Default>
@@ -200,6 +223,8 @@ export default {
 
 
 <style lang="scss" scoped>
+@use '../style/partials/variables.scss' as *;
+
 // TEMPORANEO
 .spec {
     &::after {
@@ -223,19 +248,25 @@ export default {
     object-fit: cover;
     object-position: center;
     transition: all 300ms cubic-bezier(.64,0,.28,.98);
+    border-radius: 10px;
 
     &:hover {
-        transform: scale(110%);
-        box-shadow: 0px 9px 37px 4px rgba(0,0,0,0.82);
+        transform: scale(104%);
+        box-shadow: 0px 9px 37px 4px rgba(0,0,0,0.55);
         cursor: pointer;
     }
 }
+
+.icon-color {
+        color: $primary-color;
+    }
 
 .ms_col-5{
     max-height: 500px;
 }
 .ms_col-7 {
     max-height: 500px;
+    // margin-left: -0.75rem;
 }
 
 .extra-image-container {
@@ -248,29 +279,56 @@ export default {
     &.img-2 {
         width: 100%;
         height: 50%;
+        // padding-top: 0.25rem;
+        &:first-child {
+            // padding-bottom: 0.25rem;
+            padding-top: 0;
+        }
     }
 
     &.img-3 {
         width: 50%;
         height: 50%;
+        // padding-top: 0.25rem;
     }
     &.img-3:first-child {
         width: 100%;
         height: 50%;
+        // padding-bottom: 0.25rem;
+        padding-top: 0;
     }
 
     &.img-4 {
         width: 50%;
         height: 50%;
+        // padding-top: 0.25rem;
+        &:first-child {
+            padding-top: 0;
+            // padding-bottom: 0.25rem;
+            & + div {
+                padding-top: 0;
+                // padding-bottom: 0.25rem;
+            }
+        }
     }
 
     &.img-5 {
         width: 50%;
         height: calc(100% / 3);
+        // padding-top: 0.25rem;
     }
     &.img-5:first-child {
         width: 100%;
         height: calc(100% / 3);
+        padding-top: 0;
+        // padding-bottom: 0.25rem;
+
+        & + div {
+            // padding-bottom: 0.25rem;
+            & + div {
+                // padding-bottom: 0.25rem;
+            }
+        }
     }
 
     .extra-image {
@@ -280,9 +338,73 @@ export default {
     }
 }
 
-#map-container {
-    width: 400px;
-    height: 400px;
+.specs-icon {
+    border: 1px solid $primary-color;
+    padding: 8px;
+    border-radius: 15px;
+    color: $secondary-color;
+    margin-right: 10px;
+}
 
+.info-container {
+    border: 2px solid white;
+    padding: 15px;
+    border-radius: 15px;
+    color: white;
+    margin-bottom: 36px;
+    // color: $secondary-color;
+}
+
+#map-container {
+    width: 100%;
+    height: 400px;
+    border-radius: 15px;
+}
+
+.aside-container {
+    padding: 10px;
+    background-color: $primary-color;
+    border-radius: 15px;
+    margin-bottom: 50px;
+    display: flex;
+    flex-direction: column;
+}
+
+.messages-form {
+    padding: 25px;
+    border: 2px solid white;
+    border-radius: 15px;
+    color: white;
+
+    
+    h4 {
+        margin-bottom: 25px;
+    }
+
+    input {
+        border: none;
+        background-color: rgb(226, 226, 226);
+        padding: 10px;
+        border-radius: 10px;
+        width: 100%;
+        margin-bottom: 20px;
+    }
+
+    textarea {
+        border: none;
+        background-color: rgb(226, 226, 226);
+        padding: 10px;
+        border-radius: 10px;
+        width: 100%;
+        margin-bottom: 20px;
+    }
+
+    button {
+        border: none;
+        background-color: $secondary-color;
+        color: white;
+        padding: 5px 15px;
+        border-radius: 999px;
+    }
 }
 </style>
